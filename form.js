27807f8c01593210da4b41e49aff3e6f10588afc
chmielewskigianpiero <chmielewskigianpiero@gmail.com>
2023-10-03 18:06:38 +0100

    ADD - form.js completed

---
+++ form.js
@@ -0,0 +1,33 @@
+// - chiedere all'utente il suo nome:
+//   - dichiarare una variabile
+let userName
+//   - assegnare alla variabile il valore restituito da un prompt
+userName = prompt('Il tuo nome')
+console.log(userName)
+
+
+// - chiedere all'utente il suo cognome:
+//   - dichiarare una variabile
+let surName
+//   - assegnare alla variabile il valore restituito da un prompt 
+surName = prompt('Il tuo cognome')
+console.log(surName)
+
+
+// - chiedere all'utente il suo colore preferito:
+//   - dichiarare una variabile
+let favoriteColor
+//   - assegnare alla variabile il valore restituito da un prompt 
+favoriteColor = prompt('Il tuo colore preferito')
+console.log(favoriteColor)
+
+// - chiedere all'utente la sua età:
+//   - dichiarare una variabile
+let userAge
+//   - assegnare alla variabile il valore restituito da un prompt 
+userAge = prompt ("La tua età")
+
+// - scrivere sulla pagina il risultato della concatenazione:
+let infoTogheter = userName + surName + favoriteColor + userAge  
+console.log(infoTogheter)
+
